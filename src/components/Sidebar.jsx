0c67fb2b273c1fceefe07727da5ae9ff62393cac
Peter Khalil <rikokamil97@gmail.com>
2023-11-20 18:33:59 +0200

    Edit sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { FaWindowMinimize } from "react-icons/fa";
 import { close } from "../redux/slices/toggleSlice";
@@ -10,21 +10,31 @@ import { setCurrentChat } from "../redux/slices/currentChatSlice";
 const Sidebar = () => {
   const dispatch = useDispatch();
   return (
-    <aside className="w-[70%] md:w-[250px] h-full flex flex-col pl-3 py-3 bg-background text-foreground">
-      <FaWindowMinimize
-        onClick={() => dispatch(close())}
-        className="self-end mb-3 mr-3 cursor-pointer"
-      />
-      <button className="w-full self-center border-2 bg-inherit mr-3 py-2 rounded-md mb-5 font-semibold hover:bg-accent">
-        +New Chat
-      </button>
-      <section className="w-full h-screen overflow-auto flex flex-col gap-2">
+    <aside className="w-[70%] md:w-[250px] h-full flex flex-col pl-3 pt-3 bg-background text-foreground relative">
+      {/* TOP */}
+      <section className="flex flex-col gap-2 w-full items-end pr-3">
+        <FaWindowMinimize
+          onClick={() => dispatch(close())}
+          className="cursor-pointer"
+        />
+        <button className="w-full self-center border-2 bg-inherit  py-2 rounded-md mb-5 font-semibold hover:bg-accent">
+          +New Chat
+        </button>
+      </section>
+      {/* TOP */}
+
+      <section className="w-full h-screen overflow-auto flex flex-col gap-2 mb-10 pb-12">
         {/* print star emoji */}
 
         <Folder name={"Chats"} />
         <Folder name={"Star Chats"} icon={"⭐"} />
       </section>
-      <section className="py-6">bottom</section>
+      <section className="py-4 border-t flex gap-2 items-center px-3 absolute bottom-0 w-full left-0 z-40 bg-background">
+        <div className="flex items-center cursor-pointer hover:bg-primary/10 w-full rounded-md py-1 px-3 gap-2">
+          <div className="w-8 h-8 rounded-full bg-primary"></div>
+          <h1>Name</h1>
+        </div>
+      </section>
     </aside>
   );
 };
@@ -36,7 +46,7 @@ function Folder({ name, icon }) {
     <div className="w-full flex flex-col gap-2 pr-2">
       <h1
         onClick={() => setIsCollapsed((prev) => !prev)}
-        className="text-lg flex items-center font-semibold sticky top-0 bg-background border-b cursor-pointer"
+        className="text-lg bg-background z-20 flex items-center font-semibold sticky top-0 border-b cursor-pointer"
       >
         {isCollapsed ? (
           <MdOutlineExpandLess className="rotate-90" />
@@ -65,17 +75,25 @@ function Folder({ name, icon }) {
 function Item({ name, id }) {
   const currentChat = useSelector((state) => state.currentChat.currentChat);
   const dispatch = useDispatch();
+  const handleOnclick = (e) => {
+    dispatch(setCurrentChat(id));
+  };
+
   return (
     <li
-      onClick={() => dispatch(setCurrentChat(id))}
+      onClick={handleOnclick}
+      title={name}
       className={`${
         currentChat === id
           ? "bg-primary text-primary-foreground"
           : "bg-inherit text-inherit hover:bg-primary/10"
-      } py-2 px-3 rounded-md cursor-pointer whitespace-nowrap overflow-hidden`}
+      } flex items-center px-3 h-10 rounded-md cursor-pointer relative`}
     >
       <span className="mr-2">💬</span>
-      {name}
+      <p className="whitespace-nowrap overflow-hidden">{name}</p>
+      {currentChat !== id && (
+        <span className="absolute h-full w-6 right-0 bg-gradient-to-r from-transparent to-slate-50 rounded-md"></span>
+      )}
     </li>
   );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -17,29 +17,61 @@ const Home = () => {
       dispatch(open());
     }
   }, []);
+  const message =
+    "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci distinctio placeat vitae, alias illum facere repudiandae inventore dignissimos, eveniet ex aliquid eius soluta exercitationem iste, perferendis recusandae quaerat vero! Minus.";
   return (
-    <div className="w-full h-screen flex relative">
-      <div className={clsx("w-full border md:w-fit h-full absolute left-0 top-0 z-10 flex",!isOpen?"hidden":"md:flex md:relative")}>
+    <div className="w-full h-screen flex relative overflow-hidden">
+      <div
+        className={clsx(
+          "w-full border md:w-fit h-full absolute left-0 top-0 z-10 flex",
+          !isOpen ? "hidden" : "md:flex md:relative"
+        )}
+      >
         <Sidebar />
-        <div className="flex-1 h-full bg-black/80 md:hidden"></div>
+        <div onClick={()=>dispatch(close())} className="flex-1 h-full bg-black/80 md:hidden" ></div>
       </div>
 
-      <main className="flex-1 bg-background text-foreground flex relative ">
+      <main className="flex-1 bg-background text-foreground flex relative px-6 md:p-0">
         <div
           onClick={() => dispatch(open())}
           className={clsx(
-            "absolute top-3 left-3 cursor-pointer border p-1 rounded-md text-foreground",
+            "absolute top-3 left-3 z-30 cursor-pointer border p-1 rounded-md text-foreground",
             isOpen ? "hidden" : ""
           )}
         >
           <FiMaximize />
         </div>
-        <div className="w-full h-full flex justify-center items-center">
-          <h1 className="text-4xl">Hello World</h1>
+
+        <div className="w-full h-full flex gap-2 flex-row-reverse">
+          {/* PDFViewer */}
+          <div className="hidden md:flex flex-1 h-full bg-sky-100">pdf</div>
+          {/* PDFViewer */}
+
+          {/* Conversation */}
+          <div className="flex-1 h-full flex flex-col gap-3 md:m-0 overflow-auto pb-3 px-4">
+            <div className="w-full h-32 bg-slate-400 flex justify-end">
+              <div className="h-12 w-60 bg-black"></div>
+            </div>
+            <div className="w-full h-32 bg-slate-400 flex justify-start">
+              <div className="h-12 w-60 bg-black"></div>
+            </div>
+          </div>
+          {/* Conversation */}
         </div>
       </main>
     </div>
   );
 };
-
+function Conversation({ name = "Me", message }) {
+  return (
+    <div
+      className={clsx(
+        "max-w-[70%] w-fit flex gap-2 items-start px-3 py-2 rounded-md"
+      )}
+    >
+      <h1 className="capitalize">{name}:</h1>
+      <p className="text-sm">{message}</p>
+    </div>
+  );
+}
 export default Home;
