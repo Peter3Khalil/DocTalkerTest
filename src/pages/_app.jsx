d7900aeffe118b4db8001843e558d39976eca3c2
Peter Khalil <rikokamil97@gmail.com>
2023-11-21 22:02:14 +0200

    Handle fullscreen on mobile

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,14 +1,10 @@
 "use client"
 import "../styles/globals.css";
-import { Provider, useSelector } from "react-redux";
+import { Provider } from "react-redux";
 import { store } from "../redux/store";
 import Layout from "../components/Layout";
-import { useEffect } from "react";
 // JavaScript
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    document.documentElement.webkitRequestFullscreen();
-  }, []);
   return (
     <Provider store={store}>
       <Layout>
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -5,7 +5,6 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-      <meta name="apple-mobile-web-app-capable" content="yes"/>
       </Head>
       <body>
         <Main />
