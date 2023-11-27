e001cb2456a086a4861eef2f5b8000e655f7c411
Peter Khalil <rikokamil97@gmail.com>
2023-11-27 18:08:52 +0200

    Handle Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,7 @@
 import autosize from "autosize";
 import React, { useEffect, useRef, useState } from "react";
 import { BiSolidSend } from "react-icons/bi";
+import { MdClear } from "react-icons/md";
 
 const Form = () => {
   const [value, setValue] = useState("");
@@ -9,29 +10,49 @@ const Form = () => {
   const handleOnChange = (e) => {
     setValue(e.target.value);
   };
-
-  useEffect(() => {
-    autosize(textareaRef.current);
-  }, []);
+  const handleOnkeyUp = (e) => {
+    textareaRef.current.style.height = "50px";
+    let height = e.target.scrollHeight;
+    textareaRef.current.style.height = `${height}px`;
+  };
+  const handleOnFocus = (e) => {
+      const inputContainer = document.querySelector(".inputContainer");
+      inputContainer.classList.add("border-primary");
+  }
+  const handleOnBlur = (e) => {
+      const inputContainer = document.querySelector(".inputContainer");
+      inputContainer.classList.remove("border-primary");
+  }
+  const clear = ()=>{
+      setValue("");
+      textareaRef.current.style.height = "50px";
+  }
   console.log("Form");
   return (
-    <form className="w-full p-4">
-      <div className="flex h-full w-full items-center rounded">
+    <form className="flex w-full items-center p-4">
+      <div className="w-full flex items-center rounded-es rounded-ss border-2 inputContainer pr-2">
         <textarea
+        id="textarea"
           ref={textareaRef}
-          className="max-h-[150px] flex-1 flex-grow resize-y rounded-es rounded-ss border-2 bg-transparent p-2 text-foreground outline-none focus:border-primary"
+          className="h-[50px] max-h-[150px] flex-1 resize-none  bg-transparent p-2 text-foreground outline-none"
           placeholder="Type a message"
           value={value}
           onChange={handleOnChange}
+          onKeyUp={handleOnkeyUp}
+          onFocus={handleOnFocus}
+          onBlur={handleOnBlur}
         ></textarea>
-        <button
-          type="button"
-          className="h-full flex-shrink-0 rounded-ee rounded-se bg-primary p-2 disabled:opacity-50"
-          disabled={!value.trim()}
-        >
-          <BiSolidSend className="text-2xl text-primary-foreground" />
+        <button type="button" className={`${value?"block":"hidden"}`}>
+          <MdClear className="text-xl opacity-30 text-foreground" onClick={clear}/>
         </button>
       </div>
+      <button
+        type="button"
+        className="h-full flex-shrink-0 rounded-ee rounded-se bg-primary p-2 disabled:opacity-50"
+        disabled={!value.trim()}
+      >
+        <BiSolidSend className="text-2xl text-primary-foreground" />
+      </button>
     </form>
   );
 };
