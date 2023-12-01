2080d6c82406798ec7d4e42cdcf567105eab8f27
Peter Khalil <rikokamil97@gmail.com>
2023-12-01 22:30:15 +0200

    Refactor

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -19,10 +19,13 @@ const Home = () => {
 const Chats = ({ chats }) => {
   return (
     <div className="h-full w-full">
-      <div className="h-14 w-full shrink-0 border">Stars</div>
+      <div className="h-14 w-full shrink-0 border-b flex items-center justify-between px-3 cursor-pointer hover:bg-accent hover:text-accent-foreground">
+        <h1 className="flex items-center"><span className="text-sm mr-1">⭐</span>Stars</h1>
+       <span className="text-sm">5</span>
+      </div>
       <ul className="w-full">
-      {Array.from({ length: 20 }).map((item) => (
-        <ChatItem />
+      {Array.from({ length: 20 }).map((item,i) => (
+        <ChatItem key={i}/>
       ))}
       </ul>
     </div>
@@ -30,12 +33,12 @@ const Chats = ({ chats }) => {
 }
 const ChatItem = ({ chat }) => {
   return (
-    <li className="flex items-center justify-between border-b px-3 py-2">
-      <div className="flex items-center gap-2">
-        <div className="h-8 w-8 rounded-full bg-black"></div>
-        <h1>ChatName</h1>
+    <li className="flex w-full gap-2 cursor-pointer items-center justify-between border-b px-3 h-14 cursor-pointer hover:bg-accent hover:text-accent-foreground">
+      <div className="flex items-center gap-2 w-[90%]">
+        <div className="h-8 w-8 rounded-full bg-black shrink-0"></div>
+        <h1 className="overflow-hidden text-ellipsis whitespace-nowrap">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium repellendus quia dolorem, ratione quo fuga aspernatur beatae consequuntur. Doloremque odio distinctio facere illo adipisci nihil iure maiores aliquid tempora alias.</h1>
       </div>
-      <CiMenuKebab />
+      <CiMenuKebab className=""/>
     </li>
   )
 }
