016a11bb3dbf5eed26fb28495d18bed7a2e8e49f
Peter Khalil <rikokamil97@gmail.com>
2023-12-01 22:45:59 +0200

    Refactor

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,42 @@
 import React, { useEffect, useState } from "react"
 import { CiMenuKebab } from "react-icons/ci"
 const Header = () => {
+  const [searchOpened, setSearchOpened] = useState(false)
+  useEffect(() => {
+    if (searchOpened) document.getElementById("search").focus()
+  }, [searchOpened])
   return (
     <header
-      className={`flex h-16 w-full shrink-0 items-center justify-between border-b bg-background px-3`}
+      className={`relative flex h-16 w-full shrink-0 items-center justify-between border-b bg-background px-3`}
     >
       <h1>DocTalker</h1>
       <div className="flex gap-3">
-        <button className="text-lg">🔍</button>
+        <button
+          onClick={() => setSearchOpened(true)}
+          className={`text-lg ${searchOpened ? "hidden" : "block"}`}
+        >
+          🔍
+        </button>
         <button className="text-lg">
           <CiMenuKebab />
         </button>
       </div>
+      {searchOpened && (
+        <div className="absolute left-0 top-0 flex h-full  w-full items-center bg-background">
+          <input
+            type="text"
+            className="h-full w-full px-3 outline-none"
+            placeholder="Search"
+            id="search"
+          />
+          <button
+            className="w-12 text-lg"
+            onClick={() => setSearchOpened(false)}
+          >
+            ❌
+          </button>
+        </div>
+      )}
     </header>
   )
 }
