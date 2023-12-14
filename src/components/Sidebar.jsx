473394a49b79d04d3d55b4f08f34fb2cd3cc9bcb
Peter Khalil <rikokamil97@gmail.com>
2023-12-14 16:00:57 +0200

    Creating chat page

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,40 +1,86 @@
 import React, { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import cn from "../utils/cn"
-import { IoMdClose } from "react-icons/io";
-import {MdKeyboardArrowLeft} from "react-icons/md"
+import { IoMdClose } from "react-icons/io"
+import { MdKeyboardArrowLeft } from "react-icons/md"
 import Link from "next/link"
 import { close } from "../redux/slices/isSidebarOpened"
-import { btnStyle } from "./UploadFile";
+import { btnStyle } from "./UploadFile"
+import { useRouter } from "next/router"
 const Sidebar = (props) => {
-  const isSidebarOpened = useSelector((state) => state.isSidebarOpened.isSidebarOpened)
+  const isSidebarOpened = useSelector(
+    (state) => state.isSidebarOpened.isSidebarOpened,
+  )
   const dispatch = useDispatch()
   const closeSidebar = () => {
     dispatch(close())
   }
+  const router = useRouter()
+  const { chatId } = router.query
   return (
-    <aside {...props} className={cn("absolute flex left-0 order-1 transition-transform duration-300 ease-in-out h-full w-full shrink-0 md:static md:w-[250px]",{
-      "translate-x-0":isSidebarOpened,
-      "-translate-x-full md:fixed":!isSidebarOpened,
-    })}>
-      <div className="flex flex-col w-full h-full bg-foreground text-background dark:bg-background dark:text-foreground">
-        <section className="flex items-center justify-center w-full px-2 py-3">
-          <Link href={"/"} className={cn(btnStyle," w-full text-md")} title="New Chat" >
+    <aside
+      {...props}
+      className={cn(
+        "absolute left-0 order-1 flex h-full w-full shrink-0 transition-transform duration-300 ease-in-out md:static md:w-[250px]",
+        {
+          "translate-x-0": isSidebarOpened,
+          "-translate-x-full md:fixed": !isSidebarOpened,
+        },
+      )}
+    >
+      <div className="flex h-full w-full flex-col bg-foreground text-background dark:bg-background dark:text-foreground">
+        <section className="flex w-full items-center justify-center px-2 py-3">
+          <Link
+            href={"/"}
+            className={cn(btnStyle, " w-full text-md")}
+            title="New Chat"
+          >
             + New Chat
           </Link>
         </section>
 
-        
-        <section></section>
+        <section className="flex w-full flex-grow flex-col overflow-auto px-2 gap-1">
+          <h2 className="text-muted/70 dark:text-muted-foreground/70 text-sm px-2">Chats</h2>
+          <ul className=" chats flex h-full w-full flex-col gap-2 overflow-auto pr-2">
+            {Array.from({ length: 70 }).map((_, i) => (
+              <li
+                key={i}
+                className={cn(
+                  "relative w-full rounded px-2 py-1 hover:bg-muted/20 dark:hover:bg-muted",
+                  {
+                    "bg-primary hover:bg-primary dark:hover:bg-primary":
+                      chatId == i,
+                  },
+                )}
+              >
+                <Link href={`/chat/${i}`}>
+                  <p className="h-full w-full overflow-hidden whitespace-nowrap text-sm ">
+                    ChatChatChatChatChatChatChatChatChatChatChatChatChatChatChatChatChatChatChatChatChatChatChatChatChatChatChatChatChatChatChatChatChatChat{" "}
+                    {i}
+                  </p>
+                </Link>
+                {chatId != i && (
+                  <div className="absolute bottom-0 right-0 top-0 w-8 bg-gradient-to-l from-foreground/50 to-transparent  dark:from-background/50"></div>
+                )}
+              </li>
+            ))}
+          </ul>
+        </section>
 
-        
-        <section></section>
+        <section className="h-16 w-full shrink-0 bg-black"></section>
       </div>
-      
-      
-      <div className="flex items-start w-32 h-full p-2 bg-transparent shrink-0 md:hidden" onClick={closeSidebar}>
-        <button title="Close sidebar" type="button" className="text-2xl border border-foreground" onClick={closeSidebar}>
-          <IoMdClose  />
+
+      <div
+        className="flex h-full w-32 shrink-0 items-start bg-transparent p-2 md:hidden"
+        onClick={closeSidebar}
+      >
+        <button
+          title="Close sidebar"
+          type="button"
+          className="border border-foreground text-2xl"
+          onClick={closeSidebar}
+        >
+          <IoMdClose />
         </button>
       </div>
     </aside>
--- src/components/UploadFile.jsx
+++ src/components/UploadFile.jsx
@@ -13,16 +13,17 @@ const UploadFile = () => {
   const [isProcessing, setIsProcessing] = useState(false)
   const supportedFileTypes = ["pdf"]
   const fileRef = useRef(null)
-  const notify = () => toast.success('File Uploaded and processed successfully', {
-    position: "top-right",
-    autoClose: 1000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "light",
-    });
+  const notify = () =>
+    toast.success("File Uploaded and processed successfully", {
+      position: "top-right",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    })
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0])
@@ -36,14 +37,15 @@ const UploadFile = () => {
     try {
       const res = await fetch("http://localhost:5000/upload/upload", {
         method: "POST",
-        headers:{
-          "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc5MWQ4NTZlMTg1Y2FhYTlkZWRmYzAiLCJpYXQiOjE3MDI0MzY2NzEsImV4cCI6MTcwMjYwOTQ3MX0.kC3rSzJffb77JIVO1Vk1CS1lZ_Xn1Pegkf26gM0z9p4"
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc5MWQ4NTZlMTg1Y2FhYTlkZWRmYzAiLCJpYXQiOjE3MDI0MzY2NzEsImV4cCI6MTcwMjYwOTQ3MX0.kC3rSzJffb77JIVO1Vk1CS1lZ_Xn1Pegkf26gM0z9p4",
         },
         body: formData,
       })
       const data = await res.json()
       console.log(data)
-      if(data.error){
+      if (data.error) {
         setIsProcessing(false)
         toast.error(data.error, {
           position: "top-right",
@@ -54,16 +56,16 @@ const UploadFile = () => {
           draggable: true,
           progress: undefined,
           theme: "light",
-          });
-          return
+        })
+        return
       }
       if (data) {
         const response = await fetch("http://localhost:5000/upload/process", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
-            "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc5MWQ4NTZlMTg1Y2FhYTlkZWRmYzAiLCJpYXQiOjE3MDI0MzY2NzEsImV4cCI6MTcwMjYwOTQ3MX0.kC3rSzJffb77JIVO1Vk1CS1lZ_Xn1Pegkf26gM0z9p4"
-
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc5MWQ4NTZlMTg1Y2FhYTlkZWRmYzAiLCJpYXQiOjE3MDI0MzY2NzEsImV4cCI6MTcwMjYwOTQ3MX0.kC3rSzJffb77JIVO1Vk1CS1lZ_Xn1Pegkf26gM0z9p4",
           },
           body: JSON.stringify({ id: data.chatId }),
         })
@@ -83,7 +85,7 @@ const UploadFile = () => {
     }
   }
   return (
-    <main className="flex items-center justify-center w-full h-full px-4">
+    <main className="flex h-full w-full items-center justify-center px-4">
       <ToastContainer
         position="top-right"
         autoClose={1000}
@@ -100,22 +102,22 @@ const UploadFile = () => {
       <ToastContainer />
 
       {isProcessing && (
-        <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full bg-muted/50">
+        <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center bg-muted/50">
           <div className="flex h-[400px] w-[400px] flex-col items-center justify-center rounded border bg-background">
-            <IoIosSettings className="text-4xl leading-none animate-spin text-primary" />
+            <IoIosSettings className="animate-spin text-4xl leading-none text-primary" />
             <p className="text-xl font-bold text-foreground">Processing</p>
           </div>
         </div>
       )}
 
       {!isProcessing && (
-        <div className="flex h-[400px] w-full flex-col items-center justify-center gap-4 rounded border-2 border-accent-foreground/20 border-dashed bg-muted/20 p-6 md:w-[400px]">
+        <div className="flex h-[400px] w-full flex-col items-center justify-center gap-4 rounded border-2 border-dashed border-accent-foreground/20 bg-muted/20 p-6 md:w-[400px]">
           {file ? (
             <FaFileUpload className="text-4xl leading-none text-primary" />
           ) : (
             <FaCloudUploadAlt className="text-4xl leading-none text-primary" />
           )}
-          <p className="w-full overflow-hidden text-xl font-bold text-center text-ellipsis whitespace-nowrap text-foreground">
+          <p className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-center text-xl font-bold text-foreground">
             {file ? file.name : "Choose a file to chat with"}
           </p>
           <form className="flex items-center gap-2" onSubmit={handleOnsubmit}>
@@ -142,12 +144,13 @@ const UploadFile = () => {
             </label>
           </form>
           <div className="flex items-center gap-1">
-            <span className="text-sm text-foreground/70">
-              Supported file:
-            </span>
+            <span className="text-sm text-foreground/70">Supported file:</span>
             <ul className="flex items-center gap-2 text-[10px] ">
               {supportedFileTypes.map((type) => (
-                <li key={type} className="flex items-center justify-center px-2 py-1 uppercase rounded bg-muted">
+                <li
+                  key={type}
+                  className="flex items-center justify-center rounded bg-muted px-2 py-1 uppercase"
+                >
                   {type}
                 </li>
               ))}
